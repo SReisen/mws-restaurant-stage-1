8dc4908a5d63f8cfd069f9df6d589ba40a08ea6b
Michael Dittmer <mditterm-reg@yahoo.de>
2018-04-04 12:28:40 +0200

    canary runs

--- sw.js
+++ sw.js
@@ -1,80 +1,69 @@
-let staticCacheName = 'restaurant-static-v3';
-let contentImgsCache = 'restaurant-content-imgs';
-let allCaches = [staticCacheName, contentImgsCache];
+let siteCache = 'restaurant-sw-v2';
+let siteCacheUrls = [
+    '/',
+    'index.html',
+    'sw.js',
+    'restaurant.html',
+    'css/styles.css',
+    'js/dbhelper.js',
+    'js/main.js',
+    'js/restaurant_info.js',
+    'data/restaurants.json'
+    ];
 
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open(staticCacheName).then(function(cache) {
-            return cache.addAll([
-                '/',
-                'index.html',
-                'restaurant.html',
-                'css/styles.css',
-                'js/dbhelper.js',
-                'js/main.js',
-                'js/restaurant_info.js',
-                'data/restaurants.json'
-            ]);
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open(siteCache).then(function(cache) {
+            return cache.addAll(siteCacheUrls);
         })
     );
 });
 
-self.addEventListener('activate', function(event) {
-    event.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(
-                cacheNames.filter(function(cacheName) {
-                    return cacheName.startsWith('restaurant-') && cacheName != staticCacheName;
-                }).map(function(cacheName) {
-                    return caches.delete(cacheName);
-                })    
-            );
-        })
-    );
-});
+self.addEventListener('fetch', function(e) {
+    let reqUrl = new URL(e.request.url);
+    if (reqUrl.origin === location.origin) {  
+        // caches images without size information 
+        if (reqUrl.pathname.startsWith('/images/')){
+           let imageUrl = reqUrl.toString().substr(0, reqUrl.toString().length-10).concat(".jpg");
+           console.log(imageUrl);
+            e.respondWith(
 
-self.addEventListener('fetch', function(event){
-    let requestUrl = new URL(event.request.url);
-  
-    if (requestUrl.origin === location.origin) {    
-        if (requestUrl.pathname.startsWith('/images/')) {
-            event.respondWith(serveImg(event.request));
-            return;
+            caches.open(siteCache).then(function(cache) {   
+                return cache.match(imageUrl).then(function(res) {
+                    if (res)
+                        return res; 
+                 
+                    //return caches.open(siteCache).then(function(cache) {
+                    return fetch(e.request).then(function(nRes) {
+                        cache.put(imageUrl, nRes.clone());
+                        return nRes; 
+                    })
+                
+                })
+            })
+            )
+
         }
-        if (requestUrl.pathname.startsWith('/restaurant.html')) {
-            event.respondWith(
-                caches.match(event.request, {ignoreSearch: true}).then(function(response) {
-                    if (response) return response;
-                    return fetch(event.request)
-                }))
+        // ignores the URL appendix at the restaurant.html sites
+        else if (reqUrl.pathname.startsWith('/restaurant.html')){
+            e.respondWith(
+                caches.match(e.request, {ignoreSearch: true}).then(function(res) {
+                    if (res) return res;
+                    return fetch(e.request)
+                })
+
+            )
+        }
+    
+        else {e.respondWith(
+            caches.match(e.request).then(function(res){
+                if (res) return res;
+                return fetch(e.request)
+            })
+        )
         }
     }
-
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            if (response) return response;
-            return fetch(event.request)
-        })
-    );
 });
 
-function serveImg(request) {
-    let storageUrl = request.url.replace(/-\d+px\.jpg$/, '');
 
-    return caches.open(contentImgsCache).then(function(cache) {
-        return cache.match(storageUrl).then(function(response) {
-            if (response)
-                return response;
 
-            return fetch(request).then(function(networkResponse) {
-                cache.put(storageUrl, networkResponse.clone());
-                return networkResponse;
-            });
-        });
-    });
-}
-self.addEventListener('message', function(event) {
-    if (event.data.action === 'skipWaiting') {
-       self.skipWaiting();
-    }
-});
