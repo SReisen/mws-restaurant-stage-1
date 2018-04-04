a845dc09807e676e02fe3ad3e08f49bf5d82406b
Michael Dittmer <mditterm-reg@yahoo.de>
2018-04-05 00:09:40 +0200

    changed semantic order of  'h elements' index.html: h1 title, h2 filter options, h3 restaurant name restaurant.html: h1 title, h2 restaurant name, h3 reviews

--- js/main.js
+++ js/main.js
@@ -146,7 +146,7 @@ createRestaurantHTML = (restaurant) => {
   image.src = DBHelper.imageUrlForRestaurant(restaurant).replace('.jpg', '-300px.jpg');
   li.append(image); 
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h3');
   name.innerHTML = restaurant.name;
   li.append(name);
 
